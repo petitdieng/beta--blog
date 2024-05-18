d2e8155e7e70cc4ec49e71b08814a9acd7904545
petitdieng <petitdieng@gmail.com>
2024-05-18 13:20:04 +0000

    bootstrap ok esbuild ok

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+// then, add these lines in app/javascript/application.js
+import jquery from 'jquery'
+// these lines add jQuery and $ to global variables so you can use $()
+window.jQuery = jquery
+window.$ = jquery
