ecc0139298a66f4902e84c9edb5661a2b31cacbb
petitdieng <petitdieng@gmail.com>
2024-05-22 10:44:16 +0000

    bootstraping beta-blog

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,3 +8,5 @@ import jquery from 'jquery'
 // these lines add jQuery and $ to global variables so you can use $()
 window.jQuery = jquery
 window.$ = jquery
+
+import "@fortawesome/fontawesome-free/js/all";
